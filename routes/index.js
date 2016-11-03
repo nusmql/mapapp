50602749e4150ac80f9436397787393b686653d2
U-SG\Jack.Meng <Jack.Meng@SG217.sg.bwoil.com>
2016-11-03 18:17:48 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var fhip = "http://192.168.68.20/chartering-api/frontend/web/index.php";
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {title: 'Map - This is important for the SEO'});
+});
+
+
+module.exports = router;
