c5660a0544f197cea0a014b0787e4cf716fad120
U-SG\Jack.Meng <Jack.Meng@SG217.sg.bwoil.com>
2016-11-04 16:39:38 +0800

    testing control function

--- public/javascripts/mousePosition/L.Control.MousePosition.js
+++ public/javascripts/mousePosition/L.Control.MousePosition.js
@@ -37,6 +37,7 @@ L.Map.mergeOptions({
 });
 
 L.Map.addInitHook(function () {
+  console.log(this.options);
     if (this.options.positionControl) {
         this.positionControl = new L.Control.MousePosition();
         this.addControl(this.positionControl);
